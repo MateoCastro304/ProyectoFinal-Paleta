e31abb16e5b0ac53b49b4c853c297803a644c5d4
MateoCastro304 <matecr361@gmail.com>
2023-08-28 17:23:32 -0300

    Software de alimentacion funcionando correctamente. PROBAR

--- Alimentacion-Paleta/src/main.cpp
+++ Alimentacion-Paleta/src/main.cpp
@@ -1,38 +1,37 @@
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
-
-#define lcdAdd 0x27
-const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
-LiquidCrystal_I2C lcd(lcdAdd,16,2);
+//#define lcdAdd 0x27
+//const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
+//LiquidCrystal_I2C lcd(lcdAdd,16,2);
 
 void setup() {
-  pinMode(A0,INPUT);
-  pinMode(D5,OUTPUT);
-
-  digitalWrite(D5,HIGH);
-  Wire.begin(D1,D2);
-  lcd.init();
+  Serial.begin(9600);
   delay(10);
-  lcd.backlight();
-  lcd.clear();
+
+  pinMode(A0,INPUT);
+  pinMode(D6, OUTPUT);          // Configura el pin del LED como salida
+  pinMode(D7, OUTPUT);
+
+  digitalWrite(D6,HIGH);
+
+  //Wire.begin(D1,D2);
+  //lcd.init();
+  // delay(10);
+  // lcd.backlight();
+  // lcd.clear();
 
   // initialize serial communication at 115200
-  Serial.begin(9600);
+  
 }
 
 void loop() {
   // read the analog in value
-  
-  
-  static const int interval=5000;
+  /*static const int interval=5000;
   static int previousMillis=0;
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     // Guardar el tiempo actual
     lcd.clear();
-    
     int analogValue = analogRead(analogInPin);
-  
     float BatRead = ( (float)analogValue * 4.84)/1023.0;
     // print the readings in the Serial Monitor
     lcd.setCursor(0,0);
@@ -43,5 +42,7 @@ void loop() {
     lcd.print(BatRead);
 
     previousMillis = currentMillis;
-  }
+  }*/
+  digitalWrite(D7,!digitalRead(D7));
+  delay(200);
 }
