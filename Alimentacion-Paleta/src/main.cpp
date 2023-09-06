decb172a7726876d47093835b717b6b9753ac5e9
MateoCastro304 <matecr361@gmail.com>
2023-09-06 17:02:17 -0300

    Alimentacion cambiada para prueba de circuito Mosfet y botón

--- Alimentacion-Paleta/src/main.cpp
+++ Alimentacion-Paleta/src/main.cpp
@@ -1,48 +1,59 @@
 #include <Arduino.h>
-//#define lcdAdd 0x27
-//const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
-//LiquidCrystal_I2C lcd(lcdAdd,16,2);
+#include <LiquidCrystal_I2C.h>
+
+// Informacion sobre puertos GPIO ESP8266
+/* https://randomnerdtutorials.com/esp8266-pinout-reference-gpios/ */
+
+const byte STAT = 13;
+const byte ledTest = 14;
+const int analogInPin = 17;  // ESP8266 Analog Pin ADC0 = 17
+
+#define lcdAdd 0x27
+LiquidCrystal_I2C lcd(lcdAdd,16,2);
 
 void setup() {
+  pinMode(analogInPin,INPUT);
+  pinMode(ledTest, OUTPUT);        // Configura el pin del LED como salida
+  pinMode(STAT, OUTPUT);
+
+  digitalWrite(STAT,HIGH);
+  
   Serial.begin(9600);
   delay(10);
 
-  pinMode(A0,INPUT);
-  pinMode(D6, OUTPUT);          // Configura el pin del LED como salida
-  pinMode(D7, OUTPUT);
+  Wire.begin(SDA,SCL);
 
-  digitalWrite(D6,HIGH);
-
-  //Wire.begin(D1,D2);
-  //lcd.init();
-  // delay(10);
-  // lcd.backlight();
-  // lcd.clear();
-
-  // initialize serial communication at 115200
-  
+  lcd.init();
+  delay(10);
+  lcd.backlight();
+  lcd.clear();
 }
 
 void loop() {
   // read the analog in value
-  /*static const int interval=5000;
-  static int previousMillis=0;
+  static const int interval[2]={500,8000};
+  static int previousMillis[2] = {0,0};
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis[0] >= interval[0]) {
     // Guardar el tiempo actual
-    lcd.clear();
-    int analogValue = analogRead(analogInPin);
-    float BatRead = ( (float)analogValue * 4.84)/1023.0;
-    // print the readings in the Serial Monitor
-    lcd.setCursor(0,0);
-    lcd.print("Analog= ");
-    lcd.print(analogValue);
-    lcd.setCursor(0,1);
-    lcd.print("Bat= ");
-    lcd.print(BatRead);
+    // lcd.clear();
+    // int analogValue = analogRead(analogInPin);
+    // float BatRead = ( (float)analogValue * 4.84)/1023.0;
+    // // print the readings in the Serial Monitor
+    // lcd.setCursor(0,0);
+    // lcd.print("Analog= ");
+    // lcd.print(analogValue);
+    // lcd.setCursor(0,1);
+    // lcd.print("Bat= ");
+    // lcd.print(BatRead);
+    digitalWrite(ledTest,!digitalRead(ledTest));
 
-    previousMillis = currentMillis;
-  }*/
-  digitalWrite(D7,!digitalRead(D7));
-  delay(200);
+    previousMillis[0] = currentMillis;
+  }
+  if (currentMillis - previousMillis[1] >= interval[1])
+  {
+    previousMillis[1] = currentMillis;
+    Serial.println("Sistema apagado");
+    digitalWrite(STAT, LOW);
+  }
 }
